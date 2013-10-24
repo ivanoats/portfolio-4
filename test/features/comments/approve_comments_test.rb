c1041525b9eb1ca7bd49bd4a3301a9cb01279a50
Andre Bautista <andrebautista2513@gmail.com>
2013-10-24 09:26:20 -0700

    updated travis file to get travis to pass

--- test/features/comments/approve_comments_test.rb
+++ test/features/comments/approve_comments_test.rb
@@ -8,23 +8,24 @@ feature "As an author or editor I want to approve comments so there's no spam " 
     fill_in :comment_author_email, with: "troll@example.com"
     fill_in :comment_content,      with: "trolling"
     click_on "Submit comment for approval"
+    click_on "Sign Out"
   end
 
   scenario "Comments not be displayed until approved" do
     new_comment
-
     visit post_path(posts(:sample_post))
     page.wont_have_content "trolling"
   end
 
-  # scenario "editor can approve comment" do
-  #   new_comment
-  #   save_and_open_page
-  #   sign_in_user(users(:editor))
-  #   visit post_path(posts(:sample_post))
-  #   click_on "Approve"
-  #   click_on "Sign Out"
-  #   visit post_path(posts(:sample_post))
-  #   page.must_have_content "trolling"
-  # end
+  scenario "editor can approve comment" do
+    new_comment
+    save_and_open_page
+    sign_in_user(users(:editor))
+
+    visit post_path(posts(:sample_post))
+    click_on "Approve"
+    click_on "Sign Out"
+    visit post_path(posts(:sample_post))
+    page.must_have_content "trolling"
+  end
 end
---
+++ test/features/comments/comment_on_project_test.rb
@@ -0,0 +1,26 @@
+# require "test_helper"
+
+# feature "As a site visitor I want to comment
+#   on projects so that I can troll my friends" do
+
+#   scenario "comment on projects" do
+#     #given a project
+#     #when I visit the project path
+#     visit project_path(projects(:one))
+#     fill_in :comment_author, with: "troll one"
+#     fill_in :comment_author_url, with: "http://troll.com"
+#     fill_in :comment_content,  with: "noob"
+#     click_on "Submit comment for approval"
+
+#     page.must_have_content "awaiting moderation"
+#   end
+
+#   scenario "invalid comment does not save" do
+#     #given a published post when I visit the path
+#     visit project_path(projects(:one))
+#     #fill out a comment with invalid data
+#     click_on "Submit comment for approval"
+#     #I should see errors of what's missing
+#     page.must_have_content "prohibited this comment from being saved"
+#   end
+# end
